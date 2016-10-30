fc8a77f29101acebaeccf56b73d121bc8284cfc3
Craig Barnes <Cr@igBarn.es>
2016-10-30 23:16:29 +0000

    Use pkg-config information for libmarkdown if available

--- mk/discount.mk
+++ mk/discount.mk
@@ -1,12 +1,15 @@
 DISCOUNT_SRCDIR = build/discount-2.2.1
+PKGCONFIG ?= pkg-config --silence-errors 2>/dev/null
 
 ifdef USE_LOCAL_DISCOUNT
  DISCOUNT_LDFLAGS = -Wl,-Bstatic -L$(DISCOUNT_SRCDIR) -lmarkdown -Wl,-Bdynamic
  DISCOUNT_CFLAGS = -I$(DISCOUNT_SRCDIR)
  showdown: | $(DISCOUNT_SRCDIR)/libmarkdown.a
+else ifeq '$(shell $(PKGCONFIG) --exists libmarkdown && echo 1)' '1'
+ DISCOUNT_LDFLAGS ?= $(shell $(PKGCONFIG) --libs libmarkdown)
+ DISCOUNT_CFLAGS ?= $(shell $(PKGCONFIG) --cflags libmarkdown)
 else
- # TODO: Use pkg-config
- DISCOUNT_LDFLAGS = -lmarkdown
+ DISCOUNT_LDFLAGS ?= -lmarkdown
 endif
 
 DISCOUNT_FLAGS = $(DISCOUNT_CFLAGS) $(DISCOUNT_LDFLAGS)
