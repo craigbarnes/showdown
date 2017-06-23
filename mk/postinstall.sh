b1da6f0683704aa2e86b0be7acbf5f030fb7f400
Craig Barnes <Cr@igBarn.es>
2017-06-23 19:56:03 +0100

    Add Meson post-install script

---
+++ mk/postinstall.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+DESKTOPDIR="$1"
+ICONDIR="$2"
+
+if test -z "$DESTDIR"; then
+    echo "Updating desktop database in $DESKTOPDIR"
+    update-desktop-database -q "$DESKTOPDIR"
+
+    echo "Updating icon cache in $ICONDIR"
+    touch -c "$ICONDIR"
+    gtk-update-icon-cache -qtf "$ICONDIR"
+else
+    echo 'DESTDIR is set; skipping install script'
+fi
+
+exit 0
