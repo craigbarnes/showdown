1337dcf3b0715c4a5f7a10e30ed5f6f727a5f8f9
Craig Barnes <Cr@igBarn.es>
2018-03-30 22:02:34 +0100

    Update Flatpak build to use org.gnome.Platform//3.28

--- mk/flatpak.mk
+++ mk/flatpak.mk
@@ -23,7 +23,7 @@ build/flatpak/files/bin/showdown: | $(DISCOUNT_SRCDIR)/ build/flatpak/
 	flatpak build-finish build/flatpak/ --command=showdown $(FLATPAK_PERMS)
 
 build/flatpak/: | build/
-	flatpak build-init $@ '$(APPID)' org.gnome.Sdk org.gnome.Platform 3.26
+	flatpak build-init $@ '$(APPID)' org.gnome.Sdk org.gnome.Platform 3.28
 
 public/:
 	mkdir -p $@
