4fc0bdafc07d0abd034441047ced5a7640c9e112
Craig Barnes <Cr@igBarn.es>
2020-07-15 02:33:53 +0100

    Update mk/version.sh script

--- mk/version.sh
+++ mk/version.sh
@@ -3,21 +3,20 @@ set -eu
 
 export VPREFIX="$1"
 
-git_describe_ver=$(git describe --match="$VPREFIX" 2>/dev/null || true)
+# These values are filled automatically for git-archive(1) tarballs.
+# See: "export-subst" in gitattributes(5).
+distinfo_commit_full='$Format:%H$'
+distinfo_commit_short='$Format:%h$'
+distinfo_author_date='$Format:%ai$'
 
-if test -n "$git_describe_ver"; then
-    echo "$git_describe_ver"
+if expr "$distinfo_commit_short" : '[0-9a-f]\{7,40\}$' >/dev/null; then
+    echo "${VPREFIX}-g${distinfo_commit_short}-dist"
     exit 0
 fi
 
-distinfo_ver=$(awk '
-    NR==1 && /^[a-f0-9]{40}$/ {
-        print ENVIRON["VPREFIX"] "-" substr($0,0,12) "-dist"
-    }
-' .distinfo)
-
-if test -n "$distinfo_ver"; then
-    echo "$distinfo_ver"
+git_describe_ver=$(git describe --match="$VPREFIX" 2>/dev/null || true)
+if test -n "$git_describe_ver"; then
+    echo "$git_describe_ver"
     exit 0
 fi
 
